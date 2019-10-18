bd374a343c6282d59a61f42389fdcdbf5790af7b
alexander <alexandermozhaev1@gmail.com>
2019-10-18 18:04:11 +0300

    hometask-1

--- src/main/java/ru/sbt/mipt/oop/EventDoorClosed.java
+++ src/main/java/ru/sbt/mipt/oop/EventDoorClosed.java
@@ -8,17 +8,8 @@ public class EventDoorClosed implements Event {
                 if (door.getId().equals(objectId)) {
                     door.setOpen(false);
                     System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
-                    // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-                    // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-                    if (room.getName().equals("hall")) {
-                        for (Room homeRoom : smartHome.getRooms()) {
-                            for (Light light : homeRoom.getLights()) {
-                                light.setOn(false);
-                                SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                                sendCommand(command);
-                            }
-                        }
-                    }
+                    LightOffHome lightOffHome = new LightOffHome();
+                    lightOffHome.offLightHome(room, smartHome);
                 }
             }
         }
