098193bbe7237e3d822ae8a653e4f24be011cdca
alexander <alexandermozhaev1@gmail.com>
2019-12-28 20:06:18 +0300

    Попытка 3

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -7,25 +7,8 @@ public class Application {
     public static void main(String... args) throws IOException {
         EventHandling eventHandling = new EventHandlingDoorClosedOpenLightOnOff();
         SmartHomeTakeEvent smartHomeTakeEventJson = new SmartHomeTakeEventJson("smart-home-1.js");
-        // считываем состояние дома из файла
         SmartHome smartHome = smartHomeTakeEventJson.takeSmartHome();
-        // начинаем цикл обработки событий
-        SensorEvent event = getNextSensorEvent();
-        while (event != null){
-            eventHandling.produceEvent(event, smartHome);
-            event = getNextSensorEvent();
-        }
-    }
-
-    private static void sendCommand(SensorCommand command) {
-        System.out.println("Pretent we're sending command " + command);
-    }
-
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
+        EventLoop eventLoop = new EventLoop(eventHandling, smartHome);
+        eventLoop.startCycle();
     }
 }
